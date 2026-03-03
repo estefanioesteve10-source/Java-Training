0840a408969be9dc2f098aba92e89a22d9a842a2
Estefanio Fernando <estefanioesteve10@gmail.com>
2026-03-03 23:43:54 +0000

    add

--- exercicio/src/Main.java
+++ exercicio/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Qual o teu nome? ");
+        System.out.println("What is your name? ");
         var scanner = new Scanner(System.in);
         var nome = scanner.next();
         System.out.printf("My name is %s", nome);
